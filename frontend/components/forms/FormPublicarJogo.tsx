38c7e9f3cd207bc652423e78f02d57f98a0837b3
ZagZx <azevedodvictor@gmail.com>
2026-01-29 10:04:05 -0300

    refactor: identação

--- frontend/components/forms/FormPublicarJogo.tsx
+++ frontend/components/forms/FormPublicarJogo.tsx
@@ -3,14 +3,13 @@ import Input from "../ui/Input";
 import Label from "../ui/Label";
 
 export default function FormPublicarJogo() {
-  return(
+	return(
 		<form className="">
 			<Card>
 				<Label htmlFor="nome">Nome do Jogo</Label>
 				<Input name="nome" id="nome"/>
 				<Label htmlFor="teste">Só pra teste</Label>
 				<Input name="teste" id="teste"/>
-				
 			</Card>
 		</form>
 	);
