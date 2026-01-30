c521f9b09f0411c65a4c23cff0bd487a1becba61
Joyuv <givanilsonmariz7@outlook.com>
2026-01-30 11:35:14 -0300

    preco e descricao

--- backend/models/json.py
+++ backend/models/json.py
@@ -1,16 +1,19 @@
 from pydantic import BaseModel
 
 class JsonJogoAtualizar(BaseModel):
-    id: int = 1
     nome: str = ""
     status: str = ""
     generosadicionar: list[int] = []
     generosremover: list[int] = []
+    preco: float = -1.0
+    descricao: str = ""
 
 class JsonJogoAdicionar(BaseModel):
     nome: str = ""
     status: str = ""
     generos: list[int] = []
+    preco: float = 0.0
+    descricao: str = ""
 
 class JsonGeneroAdicionar(BaseModel):
     nome: str = ""
