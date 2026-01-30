96d243037a67ce80d0129955419e2441d130ecea
Pedro Victor <azevedodvictor@gmail.com>
2026-01-30 16:15:50 -0300

    feat: campos status, descrição e preço adicionados

--- frontend/components/forms/FormPublicarJogo.tsx
+++ frontend/components/forms/FormPublicarJogo.tsx
@@ -2,17 +2,34 @@ import Button from "../ui/Button";
 import Card from "../ui/Card";
 import Input from "../ui/Input";
 import Label from "../ui/Label";
+import Select from "../ui/Select";
+import TextArea from "../ui/TextArea";
 
 export default function FormPublicarJogo() {
 	return(
 		<form className="">
-			<Card>
-				<Label htmlFor="nome">Nome do Jogo</Label>
-				<Input name="nome" id="nome"/>
-				<Label htmlFor="descricao">Descrição</Label>
-				<Input name="descricao" id="descricao"/>
-				<Label htmlFor="preco">Preço</Label>
-				<Input name="preco" id="preco"/>
+			<Card extraClassNames="w-4xl">
+				
+				<div className="grid-cols-3 grid gap-3">
+					<div className="grid">
+						<Label htmlFor="nome">Nome do Jogo</Label>
+						<Input name="nome" id="nome"/>
+					</div>
+					<div className="grid">
+						<Label htmlFor="preco">Preço</Label>
+						<Input name="preco" id="preco"/>
+					</div>
+					<div className="grid">
+						<Label htmlFor="status">Status</Label>
+						<Select name="status" id="status"/>
+					</div>
+				</div>
+				
+				<div className="grid">
+					<Label htmlFor="descricao">Descrição</Label>
+					<TextArea name="descricao" id="descricao"/>
+				</div>
+
 				<Button type="submit">Enviar</Button>
 			</Card>
 		</form>
