911e5e94f546e1fadfc025ad11f1f6e8b6cbfdba
Pedro Victor <azevedodvictor@gmail.com>
2026-01-30 16:11:49 -0300

    feat: hook para estado de montagem do componente

--- frontend/components/ui/MenuButton.tsx
+++ frontend/components/ui/MenuButton.tsx
@@ -2,39 +2,35 @@
 
 import { X } from "lucide-react";
 import { useTheme } from "next-themes";
-import { useContext, useEffect, useState } from "react";
+import { useContext} from "react";
 import Menu from "./MenuIcon";
 import { MenuContext } from "@/context/MenuContext";
+import useMounted from "@/hooks/isMounted";
 
 
 export default function MenuButton() {
-  const [ mounted, setMounted ] = useState(false);
   const { theme } = useTheme();
-  const { isOpen, collapsed, toggleMenu } = useContext(MenuContext)
+  const { isOpen, collapsed, toggleMenu } = useContext(MenuContext);
+  
+  const isMounted = useMounted();
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
+  if (!isMounted) return null;
 
   return(
-    <>
-      <div id="toggle-menu" className="transition-transform duration-500 w-7 cursor-pointer" onClick={toggleMenu}>
-        <div className={isOpen ? "block" : "hidden"}>
-          <X 
-            className="transition-transform duration-800"
-            color={ theme === "dark" ? "var(--color-emphasis)": undefined} 
-            size="2rem"
-          />
-        </div>
-        <div className={isOpen ? "hidden" : "block"}>
-          <Menu extraClassNames={`
-            ${collapsed ? "close-menu" : ""}
-            ${theme === "dark" ? "text-emphasis" : ""}
-          `} />
-        </div>
+    <div id="toggle-menu" className="transition-transform duration-500 w-7 cursor-pointer" onClick={toggleMenu}>
+      <div className={isOpen ? "block" : "hidden"}>
+        <X 
+          className="transition-transform duration-800"
+          color={ theme === "dark" ? "var(--color-emphasis)": undefined} 
+          size="2rem"
+        />
       </div>
-    </>
+      <div className={isOpen ? "hidden" : "block"}>
+        <Menu extraClassNames={`
+          ${collapsed ? "close-menu" : ""}
+          ${theme === "dark" ? "text-emphasis" : ""}
+        `} />
+      </div>
+    </div>
   );
 }
