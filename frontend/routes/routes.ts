a27492b0eb5d66b557f0e0b7d6bb23fe8fb8abd7
ZagZx <azevedodvictor@gmail.com>
2026-01-31 22:38:04 -0300

    refactor: API_URL para BACKEND_URL no .env

--- frontend/routes/routes.ts
+++ frontend/routes/routes.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.API_URL;
+const API_URL = process.env.BACKEND_URL;
 
 export const POST_ADD_JOGO_URL = () => `${API_URL}/add/jogo`;
 export const GET_JOGO_URL = (id: string) => `${API_URL}/get/jogo/${id}`;
@@ -6,3 +6,5 @@ export const DELETE_JOGO_URL = (id: string) => `${API_URL}/remove/jogo/${id}`;
 export const UPDATE_JOGO_URL = () => `${API_URL}/update/jogo`;
 
 export const GET_JOGOS_URL = () => `${API_URL}/get/jogos`;
+
+export const GET_GENEROS_URL = () => `${API_URL}/get/generos`;
